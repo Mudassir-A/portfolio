b735acc13ec6d6b567fd10e645eac333f1275832
Mohd Mudassir Ansari <mudassirimranansari@gmail.com>
2024-08-21 21:06:06 +0530

    [by melty] feat: Create initial portfolio website with responsive design

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+        strings: ['Mohd Mudassir Ansari'],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true,
+        loopCount: Infinity,
+        showCursor: true,
+        cursorChar: '|'
+    };
+
+    var typed = new Typed('#typed-name', options);
+});
+
